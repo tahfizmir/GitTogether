ab6ed8d346fb2dddf1523bf9ea232a8b98137318
tahfizmir <tahfeezmir27@gmail.com>
2025-09-05 23:40:32 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ app.use("/user",[authUser,(req,res)=>{
 app.use("/admin",authAdmin,(req,res)=>{
     res.status(200).send('admin authenticated');
 })
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send('something went wrong')
+    }
+})
 
 app.listen(3000,()=>{
     console.log("server is listening at 3000");
