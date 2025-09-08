58d172c44754f4c9aceb6090d67b64d516f4f115
tahfizmir <tahfeezmir27@gmail.com>
2025-09-08 12:49:53 +0530

    apis for the database

--- src/app.js
+++ src/app.js
@@ -4,14 +4,47 @@ const connectDB=require('./config/database.js');
 const User=require('./models/user.js');
 
 const app = express();
+app.use(express.json()); // converts req to js object
+
+// fetch particular user with email
+app.get("/user",async (req,res)=>{
+    try{
+        const userEmail=req.body.emailId;
+        const users=await User.find({emailId:userEmail});
+        if(users.length===0){
+            res.status(404).send("user not found");
+
+        }else{
+            res.send(users);
+        }
+
+    }catch(err){
+       
+        res.status(400).send("something went wrong in fetching user with error: ",err);
+    }
+})
+
+// get all the users for feed
+app.get("/feed",async (req,res)=>{
+    try{
+        
+        const users=await User.find({});
+        if(users.length===0){
+            res.status(404).send("no users");
+
+        }else{
+            res.send(users);
+        }
+
+    }catch(err){
+       
+        res.status(400).send("something went wrong in fetching users with error: ",err);
+    }
+})
 
 app.post('/signup',async (req,res)=>{
-    const dummy_data={
-        firstName:"Tahfiz",
-        lastName:"Mir",
-        emailId:"tahfeezmir27@gmail.com",
-        password:"hello",
-    }
+    const dummy_data=req.body;
+    console.log("dummy_data",dummy_data);
 
 
    try{ const newuser=new User(dummy_data);
